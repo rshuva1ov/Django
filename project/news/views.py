0ba1c5483f471b3078a94413246883ad13c8168c
Рэм Шувалов <shuvalov.rem@mail.ru>
2023-01-11 22:19:58 +0800

    success

--- project/news/views.py
+++ project/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
-from django.views.generic import DeleteView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -9,10 +9,22 @@ def news_home(request):
     return render(request, "news/news_home.html", {"news": news})
 
 
-class NewsDetailView(DeleteView):
+class NewsDetailView(DetailView):
     model = Article
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = 'news/create.html'
+    # fields = ['title', 'anons', 'full_text', 'date']
+    form_class = ArticleForm
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
 
 
 def create(request):
